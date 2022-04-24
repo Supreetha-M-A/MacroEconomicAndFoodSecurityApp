cee035b21afd89d665ba810cf166ae3fc22335b6
Netra Amrale <30404609+neetra@users.noreply.github.com>
2022-04-23 20:25:53 -0700

    agriculture

--- app/src/main/java/com/example/macroeconomicfoodsecurity/DBHandler.java
+++ app/src/main/java/com/example/macroeconomicfoodsecurity/DBHandler.java
@@ -21,6 +21,7 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String TABLE_NAME_FDIInFlowsPERCENT = "fdi_inflows";
     private static final String TABLE_NAME_FDIOutFlowsPERCENT = "fdi_outflows";
     private static final String TABLE_NAME_GDP_Agri = "gdp_agri";
+    private static final String TABLE_NAME_FertilizerCons = "fertilizer_consum";
     // below variable is for our id column.
     private static final String ID_COL = "id";
 
@@ -79,6 +80,16 @@ public class DBHandler extends SQLiteOpenHelper {
                 + USA_COUNTRY + " TEXT)";
 
         db.execSQL(query1);
+
+        Log.e("CreatNewTable", "Creating table" + TABLE_NAME_FertilizerCons);
+        query1 = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_FertilizerCons + " ("
+                + YEAR + " TEXT, "
+                + India_COUNTRY + " TEXT,"
+                + CHINA_COUNTRY + " TEXT,"
+
+                + USA_COUNTRY + " TEXT)";
+
+        db.execSQL(query1);
     }
 
     // below method is for creating a database by running a sqlite query
@@ -120,6 +131,35 @@ public class DBHandler extends SQLiteOpenHelper {
 
         // on below line we are creating a cursor with query to read data from database.
         Cursor cursorCourses = db.rawQuery("SELECT * FROM " + TABLE_NAME_GDBPERCENT
+                , null);
+
+        // on below line we are creating a new array list.
+        ArrayList<Model> courseModalArrayList = new ArrayList<Model>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+                // on below line we are adding the data from cursor to our array list.
+                courseModalArrayList.add(new Model(cursorCourses.getString(0),
+                        cursorCourses.getString(1),
+                        cursorCourses.getString(2),
+                        cursorCourses.getString(3)));
+            } while (cursorCourses.moveToNext());
+            // moving our cursor to next.
+        }
+        // at last closing our cursor
+        // and returning our array list.
+        cursorCourses.close();
+        return courseModalArrayList;
+    }
+
+    public ArrayList<Model> readFertiliserConsump() {
+        // on below line we are creating a
+        // database for reading our database.
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // on below line we are creating a cursor with query to read data from database.
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + TABLE_NAME_FertilizerCons
                 , null);
 
         // on below line we are creating a new array list.
@@ -330,6 +370,34 @@ public class DBHandler extends SQLiteOpenHelper {
         // after adding all values we are passing
         // content values to our table.
         db.insert(TABLE_NAME_GDP_Agri, null, values);
+
+        // at last we are closing our
+        // database after adding database.
+        db.close();
+    }
+
+    public void addFertilizerConsum(String[] inValues) {
+        Log.e("GDPAGRI" , inValues[0] + inValues[1]);
+        // on below line we are creating a variable for
+        // our sqlite database and calling writable method
+        // as we are writing data in our database.
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // on below line we are creating a
+        // variable for content values.
+        ContentValues values = new ContentValues();
+
+        // on below line we are passing all values
+        // along with its key and value pair.
+        Log.e("GDP AGRI" , inValues[0] + inValues[1]);
+        values.put(YEAR, inValues[0]);
+        values.put(CHINA_COUNTRY, inValues[1]);
+        values.put(India_COUNTRY, inValues[2]);
+        values.put(USA_COUNTRY, inValues[3]);
+
+        // after adding all values we are passing
+        // content values to our table.
+        db.insert(TABLE_NAME_FertilizerCons, null, values);
 
         // at last we are closing our
         // database after adding database.
