06eea81d75b64c567e9ec960884475a32041c5c4
Shruthi23 <shruthisrinivasan97@gmail.com>
2022-04-23 17:45:24 -0700

    Line chart added

--- app/src/main/java/com/example/macroeconomicfoodsecurity/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/macroeconomicfoodsecurity/ui/dashboard/DashboardFragment.java
@@ -1,11 +1,13 @@
 package com.example.macroeconomicfoodsecurity.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,24 +16,42 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.macroeconomicfoodsecurity.DashboardGraphActivity;
 import com.example.macroeconomicfoodsecurity.R;
 import com.example.macroeconomicfoodsecurity.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+public class
+DashboardFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     private FragmentDashboardBinding binding;
+    private Button show;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+//        DashboardViewModel dashboardViewModel =
+//                new ViewModelProvider(this).get(DashboardViewModel.class);
+//
+//        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//        show=binding.;
+//
+//
+//        final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
+        View v=inflater.inflate(R.layout.fragment_dashboard,container,false);
+        show=v.findViewById(R.id.button);
+        //show.setOnClickListener();
+        show.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getActivity(), DashboardGraphActivity.class);
+                startActivity(i);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+            }
+        });
+        return v;
     }
 
     @Override
